764c611d72b00e8790a3a57cfb9cea5e30d589bf
EgDan2 <134202587+EgDan2@users.noreply.github.com>
2024-11-20 22:31:46 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+// 引入必要模組
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// 提供靜態資源（例如 HTML、CSS、JS）
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 啟動伺服器
+app.listen(port, () => {
+  console.log('Server is running on http://localhost:${port}');
+});
